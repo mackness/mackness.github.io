807f02c575d12275db32f8c46b688f4b475c205b
mack solomon <mack.solomon@apple.com>
2016-09-13 21:11:09 -0700

    stringify json that gets posted to lamda mailer function

--- js/main.js
+++ js/main.js
@@ -1,46 +1,18 @@
 
 var App = {
-
-	handleFormSubmission: function() {
-		var data = {
-			"name" : document.querySelector('.contact-form__input').value ,
-			"message" : document.querySelector('.contact-form__textarea').value
-		}
-		function xdr(url, method, data, callback, errback) {
-		    var req;
-		    
-		    if(XMLHttpRequest) {
-		        req = new XMLHttpRequest();
-		        if('withCredentials' in req) {
-		            req.open(method, url, true);
-		        	req.setRequestHeader("Content-type", "application/json");
-		            req.onerror = errback;
-		            req.onreadystatechange = function() {
-		                if (req.readyState === 4) {
-		                    if (req.status >= 200 && req.status < 400) {
-		                        callback('response text',req.responseText);
-		                    } else {
-		                        errback(new Error('Response returned with non-OK status'));
-		                    }
-		                }
-		            };
-		            req.send(data);
-		        }
-		    } else if(XDomainRequest) {
-		        req = new XDomainRequest();
-		        req.open(method, url);
-				req.setRequestHeader("Content-type", "application/json");
-		        req.onerror = errback;
-		        req.onload = function() {
-		            callback(req.responseText);
-		        };
-		        req.send(data);
-		    } else {
-		        errback(new Error('CORS not supported'));
-		    }
+	handleFormSubmission: function(data) {
+		function req(url, data, callback, errback) {
+			var request = new XMLHttpRequest();
+			request.open('POST', url, true);
+			request.setRequestHeader('Content-Type', 'application/json');
+			request.send(JSON.stringify(data));
 		}
 
-		xdr('https://e8h533pg98.execute-api.us-west-2.amazonaws.com/dev/mailer', 'POST', data, function(res){console.log(res)}, function(res){console.log(res)})
+		req('https://e8h533pg98.execute-api.us-west-2.amazonaws.com/dev/mailer', data, function(res) {
+			console.log(res)
+		}, function(err) {
+			console.log('that is an error:', err)
+		})
 	},
 	
 	handleFormVis: function() {
@@ -56,11 +28,16 @@ var App = {
 
 		document.querySelector('#contact-form').addEventListener('submit', function(event) {
 			event.preventDefault();
-			this.handleFormSubmission()
+			var data = {
+				"name" : document.querySelector('.contact-form__input--name').value,
+				"email" : document.querySelector('.contact-form__input--email').value,
+				"message" : document.querySelector('.contact-form__textarea').value
+			}
+			this.handleFormSubmission(data);
 		}.bind(this))
 	}
-
 }
 
+window.onload = App.init(
 
-window.onload = App.init()
+)
