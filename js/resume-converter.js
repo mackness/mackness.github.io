e58dfdd1743e3fb14ea62e254bb2af50440344e1
mack solomon <mack.solomon@apple.com>
2016-11-16 09:16:39 -0800

    load webfont first

--- js/resume-converter.js
+++ js/resume-converter.js
@@ -2,6 +2,6 @@ var markdownpdf = require("markdown-pdf");
 var path = require('path');
 var fs = require("fs");
 
-markdownpdf().from(path.join(__dirname, '../c-resume.md')).to(path.join(__dirname, '../resume.pdf'), function () {
+markdownpdf().from(path.join(__dirname, '../resume/resume.md')).to(path.join(__dirname, '../resume/resume.pdf'), function () {
   console.log("Done")
 })
